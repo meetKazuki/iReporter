3fe0e7930afe4a270f7632b51dafeb0698138443
Omoefe Dukuye <omoefe.dukuye@gmail.com>
2018-12-05 14:39:09 +0100

    Set up directory for testing

--- api-endpoints/server.js
+++ api-endpoints/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './api-endpoints/src/routes/api-routes';
+import router from './src/routes/api-routes';
 
 const app = express();
 
@@ -7,3 +7,5 @@ app.use(router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`Server running at ${port}.`); });
+
+export default app;
