e2eba3e0ea233384ebc8a7154ebf042e6a390458
Desmond Edem <kukiito219@gmail.com>
2018-12-06 13:38:04 +0100

    chore(test): run first test
    [#162227234]

--- api/test/incidents.test.js
+++ api/test/incidents.test.js
@@ -10,8 +10,7 @@ describe('Red-flag API Integration Tests', () => {
       request(app).get('/api/v1/red-flags')
         .end((err, res) => {
           expect(res.statusCode).to.equal(200);
-          expect(res.body).to.be.a('Content-Type', 'application/json');
-          expect(res.body).to.be.empty();
+          expect(res.body).to.be.a('Object');
           done();
         });
     });
